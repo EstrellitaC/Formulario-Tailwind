f174a7f87f06114740bbbc6d7ff6a683cb108a1e
EstrellitaC <estrellita.caycedo@tecsup.edu.pe>
2024-11-11 20:01:32 -0500

    primercommit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      boxShadow: {
+        'sm': 'rgba(0, 0, 0, 0.16) 1px 1px 6px;',
+        'xl': 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px;'
+      }
+    },
+  },
+  plugins: [],
+}
